1bd4e0e28f783ab7a4ccd57d0d54e866851bbabd
Lera <valeriiakolesnikova21@gmail.com>
2023-12-05 19:23:43 +0500

    3 homework added

--- Lesson 3. SOLID principles/src/main/java/ru/geekbrains/lesson3/ServiceStation.java
+++ Lesson 3. SOLID principles/src/main/java/ru/geekbrains/lesson3/ServiceStation.java
@@ -1,6 +1,6 @@
 package ru.geekbrains.lesson3;
 
-public class ServiceStation implements Refueling, Wiping{
+public class ServiceStation implements Refueling, Wiping, CarService {
     @Override
     public void fuel(FuelType fuelType) {
 
@@ -19,5 +19,15 @@ public class ServiceStation implements Refueling, Wiping{
     @Override
     public void wipHeadlights() {
 
+    }
+
+     @Override
+    public void service(Car car) {
+
+    }
+
+    @Override
+    public void refuel(FuelType fuelType) {
+        
     }
 }
